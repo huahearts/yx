73f302c90951ef198f25bbf4e42e4d001eb7941c
huahearts <huahearts@126.com>
2020-12-21 16:07:15 +0800

    Create yx.h

---
+++ yx.h
@@ -0,0 +1,19 @@
+#ifndef YX_H_
+#define YX_H_
+#include <iostream>
+#include <string>
+class Unit 
+{
+public:
+    Unit() = default;
+    ~Unit() = default;
+    Unit(int id_,int maxHp_,int curHp, std::string name);
+    virtual void Attack() = 0;
+    virtual void Run() = 0;
+private:
+    int m_iId;
+    std::string m_sName;
+    int m_iMaxHp;
+    int m_iCurHp;
+};
+#endif
